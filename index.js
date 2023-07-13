7dea56e1fb3b839e7911a2d42e41ac479f0cf9dd
Sacarima <caroline12alex@yahoo.com>
2023-07-13 16:34:03 +0200

    File Storage setup

--- index.js
+++ index.js
@@ -10,6 +10,7 @@ import { fileURLToPath } from "url";
 import bodyParser from "body-parser";
 
 /*  CONFIGURATIONS  */
+
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
 dotenv.config();
@@ -22,3 +23,19 @@ app.use(bodyParser.json({ limit: "30mb", extended: true }));
 app.use(bodyParser.urlencoded({ limit: "30mb", extended: true }));
 app.use(cors());
 app.use("/assets", express.static(path.join(__dirname, 'public/assets')));
+
+
+/* FILE STORAGE  */
+
+const storage = multer.diskStorage({
+    destination: function (req, file, cb) {
+        cb(null, "public/assets");
+    },
+    filename: function (req, file, cb) {
+        cb(null, file.originalname);
+    },
+
+});
+
+const upload = multer({ storage })
+
